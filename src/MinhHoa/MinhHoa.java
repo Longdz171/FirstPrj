6905bc016e9c5087cad052caeee73d400a97ad95
nhetanphede2004 <lelong2004o@gmail.com>
2024-08-14 15:57:14 +0700

    LePhiLong

---
+++ src/MinhHoa/MinhHoa.java
@@ -0,0 +1,8 @@
+package MinhHoa;
+
+public class MinhHoa {
+	public static void main(String[] args) {
+		System.out.println("Hello Le Phi long!");
+		System.out.println("Welcome to the DHHTTT19A!");
+	}
+}
