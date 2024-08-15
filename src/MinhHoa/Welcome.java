53ce65c2e9aca58673f61b461e53955dd0d5bfc4
nhetanphede2004 <lelong2004o@gmail.com>
2024-08-15 18:23:07 +0700

    LePhiLong

--- src/MinhHoa/Welcome.java
+++ src/MinhHoa/Welcome.java
@@ -1,6 +1,6 @@
 package MinhHoa;
 
-public class MinhHoa {
+public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Hello Le Phi long!");
 		System.out.println("Welcome to the DHHTTT19A!");
